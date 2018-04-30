6f3657138b4598da7e2baa15fd8151341ad3aead
gabrielsilv4si@gmail.com <gabrielsilv4si@gmail.com>
2018-04-30 15:53:41 -0300

    Feita Tabela com os dados dos Empenhos

--- nbproject_1/pegarDados_Empenhos.php
+++ nbproject_1/pegarDados_Empenhos.php
@@ -4,11 +4,11 @@ $dados_Emp = json_decode(file_get_contents("http://dados.ufrn.br/api/action/data
 echo "<pre>";
 print_r($dados_Emp);
 echo "</pre>";
-*/ 
+*/
 
 function getDadosEmp(){
     
-   return json_decode(file_get_contents("http://dados.ufrn.br/api/action/datastore_search?resource_id=35d07c2e-d528-4a33-a912-6f223648eb00&limit=5000"));
+   return json_decode(file_get_contents("http://dados.ufrn.br/api/action/datastore_search?resource_id=35d07c2e-d528-4a33-a912-6f223648eb00&limit=5000"),TRUE);
 
 }
     
